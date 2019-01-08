5be7fe1366dafb7ea1b90362c2bf1a7a87ff49fa
nickdebesten <36811561+nickdebesten@users.noreply.github.com>
2019-01-08 14:52:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,59 +6,110 @@ client.on('ready', () => {
   console.log(` ${client.user.tag} is online!!!`);
 });
 
+var servers = {};
+var search = require('youtube-search');
+ 
+var opts = {
+  maxResults: 1, 
+  key: 'AIzaSyBKO09Rokq96z6-DtSbGdxw7L9R1__ujf0' 
+};
+
+
 client.on('message', message => {
-if (message.content === '?join') {
-  // Only try to join the sender's voice channel if they are in one themselves
-  if (!message.guild) {
-    message.reply("je moet wel in een speak zitten hond");
-    return;
-  };
-  if (message.member.voiceChannel) {
-    message.member.voiceChannel.join()
-      .then(connection => { // Connection is an instance of VoiceConnection
-        message.reply('laat het feest maar beginnen!! :)');
-        connection.playStream("http://stream01.iloveradio.de/iloveradio5.mp3");
-      })
-      .catch(console.log);
-  } else {
-    message.reply('je moet wel een speak zitten hond ( ik moet wel perms hebben)');
-  }
+  
+    if(message.content.startsWith("!melding")){
+      if(message.member.roles.has("531842154671112202")) {
+      var bericht = message.content.replace("!melding ", "");
+    let melding = new Discord.RichEmbed()
+    .setTitle("melding!")
+    .setThumbnail(client.user.avatarURL)
+    .setDescription(bericht)
+    .setColor("RANDOM")
+    
+    message.channel.send(melding)
+    
+    }}
+  
+  
+  
+  
+  const voiceChannel = message.member.voiceChannel;
+      var isReady = true;
+
+      if (message.content === '!join') {
+if(message.member.roles.has("531842154671112202")) {
+        if(!voiceChannel) {
+          const embedError = new Discord.RichEmbed()
+          .setTitle(`Foutmelding!`)
+          .setDescription(`Ik kan je spraakkanaal niet vinden!`)
+          .setColor(`#ff0000`)
+          .setFooter(`Zorg dat je in een spraakkanaal zit`);
+
+          message.channel.send(embedError)
+        } else if (isReady === true) {
+          voiceChannel.join().then(connection => {
+            const dispatcher = connection.playStream('http://18973.live.streamtheworld.com/RADIO538.mp3');
+
+            isReady = false;
+
+            const embed = new Discord.RichEmbed()
+            .setTitle(`ik speel muziek af`)
+            .setDescription(`Je luistert nu naar Radio 538!`)
+            .setColor(`#00ff00`)
+            .setFooter(`Yay :D`);
+
+            message.channel.send({embed});
+
+            dispatcher.on("end", end => {
+              voiceChannel.leave();
+              message.channel.send(`Tijd is op!`);
+              isReady = true;
+            });
+          });
+        } else {
+          const embedError2 = new Discord.RichEmbed()
+          .setTitle(`Foutmelding!`)
+          .setDescription(`Ik speel al muziek af!`)
+          .setColor(`#ff0000`)
+          .setFooter(`Yay :D`);
+
+          message.channel.send(embedError2);
+        }
+      } 
 }
-if(message.content == "?leave") {
-  if(message.member.voiceChannel){
-      message.member.voiceChannel.leave();
-  } else {
-      message.reply("bemoei je der niet mee hond");
-  }
+     if (message.content === "!leave") {
+if(message.member.roles.has("531842154671112202")) {
+        if(!voiceChannel) {
+          const embedError3 = new Discord.RichEmbed()
+          .setTitle(`Foutmelding!`)
+          .setDescription(`Ik kan je spraakkanaal niet vinden!`)
+          .setColor(`#ff0000`)
+          .setFooter(`Yay :D`);
+
+          message.channel.send(embedError3);
+        } else {
+          voiceChannel.leave();
+          message.channel.send(`Succesvol losgekoppeld!`);
+        }
+      }
 }
-if(message.content == "?owner leave") {
-  if (message.author.id == "254273544689680386" || message.author.id == "265789273070895104"){
-message.guild.voiceConnection.disconnect();
-  }
-}
-if(message.content.startsWith("!status")) {
-if (message.author.id == "254273544689680386" || message.author.id == "265789273070895104"){
+     if(message.content.startsWith("!play")) {
+if(message.member.roles.has("531842154671112202")) {
+      message.reply('laat het feest maar beginnen!! :)');
       var str = message.content;
-      var status = str.substr(7);
-      client.user.setActivity(status, { type: 'LISTENING'});
-      message.channel.send("de status van de bot is veranderd.");
-  } else {
-    message.channel.send("niet voor jouw");
-  }
+      var link = str.substr(8);
+      search(link, opts, function(err, results) {
+        if(err) return console.log(err);
+        const streamOptions = { seek: 0, volume: 1 };
+        message.member.voiceChannel.join().then(connection => {
+          const stream = ytdl(results[0].link, { filter : 'audioonly' });
+          const dispatcher = connection.playStream(stream, streamOptions);
+        })
+        .catch(console.error);  
+      });
+    }
 }
-if(message.content.startsWith("?play")) {
-        message.reply('laat het feest maar beginnen!! :)');
-  var str = message.content;
-  var link = str.substr(8);
-  const streamOptions = { seek: 0, volume: 1 };
-  message.member.voiceChannel.join()    .then(connection => {
-      const stream = ytdl(link, { filter : 'audioonly' });
-      const dispatcher = connection.playStream(stream, streamOptions);
-    })
-    .catch(console.error);
-  }
-    if (message.content === '?invite') {
-    message.reply('hier heb je de link om mij toe te voegen aan je server !https://discordapp.com/oauth2/authorize?client_id=427105558437298178&scope=bot als je niks hoort join dan deze discord voor hulp! https://discord.gg/sXzxZK6');
-  }
+  
 });
-client.login(process.env.TOKEN);
+
+client.login(process.env.TOKEN)
